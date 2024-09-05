20a12f0f66e2e3f08644809ccadd3760aa6c56d2
Yuhao Ren <git@yuhao.ren>
2024-09-05 22:09:35 +0800

    new file:   character_18/18-1_New_Projects.py

---
+++ character_18/18-1_New_Projects.py
@@ -0,0 +1,22 @@
+""" To get a better idea of what Django does, build a couple empty projects and 
+look at what Django creates. Make a new folder with a simple name, like tik_gram 
+or insta_tok (outside of your learning_log directory), navigate to that folder 
+in a terminal, and create a virtual environment. Install Django and run the 
+command django-admin.py startproject tg_project . (making sure to include the 
+dot at the end of the command). Look at the files and folders this command 
+creates, and compare them to Learning Log. Do this a few times, until you’re 
+familiar with what Django creates when starting a new project. Then delete the 
+project directories if you wish. """
+
+
+# I have done it in terminal
+
+# Using below commands:
+""" 
+python -m venv character_18
+mv 18-1_New_Projects.py character_18
+cd character_18
+source bin/activate
+django-admin startproject character_18_project .
+ls
+"""
