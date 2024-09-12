b6be4f0285ec714f251def4d41b8259fa82ec93e
Yuhao Ren <git@yuhao.ren>
2024-09-12 23:37:27 +0800

    new file:   character_18/18-5_Meal_Planner.py 	new file:   character_18/18-6_Pizzeria_Home_Page.py

---
+++ character_18/18-5_Meal_Planner.py
@@ -0,0 +1,15 @@
+""" Consider an app that helps people plan their meals throughout the week. Make 
+a new folder called meal_planner, and start a new Django project inside this 
+folder. Then make a new app called meal_plans. Make a simple home page for this 
+project. """
+
+
+""" 
+mkdir meal_planner
+django startproject meal_planner
+django startapp meal_plans
+python mange.py makemigrations
+python mange.py migrate
+Edit some files
+python mange.py runserver
+"""
---
+++ character_18/18-6_Pizzeria_Home_Page.py
@@ -0,0 +1,6 @@
+""" Add a home page to the Pizzeria project you started in Exercise 18-4 
+(page 388). """
+
+
+# There is something wrong with 18-4
+# ModelNotFundError
