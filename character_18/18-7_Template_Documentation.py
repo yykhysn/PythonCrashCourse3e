863422630839455284036ce46014b19713701bac
Yuhao Ren <git@yuhao.ren>
2024-09-18 23:58:35 +0800

    new file:   character_18/18-7_Template_Documentation.py 	new file:   character_18/18-8._Pizzeria_Pages.py

---
+++ character_18/18-7_Template_Documentation.py
@@ -0,0 +1,6 @@
+""" Skim the Django template documentation at 
+https://docs.djangoproject.com/en/4.1/ref/templates. You can refer back to it 
+when you’re working on your own projects. """
+
+
+# Yes, it is
---
+++ character_18/18-8._Pizzeria_Pages.py
@@ -0,0 +1,8 @@
+""" Add a page to the Pizzeria project from Exercise 18-6 (page 392) that shows 
+the names of available pizzas. Then link each pizza name to a page displaying 
+the pizza’s toppings. Make sure you use template inheritance to build your pages 
+efficiently. """
+
+
+# Sorry, there is something wrong with 18-6.
+# As a consequence, cannot finish at this time.
